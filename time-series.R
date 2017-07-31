2a8f2463f30537c7751399253a935023db41407f
Soumyadip Dutta <soumyadipdutta2007@gmail.com>
2017-08-01 01:46:50 +0530

    update in arima model

--- time-series.R
+++ time-series.R
@@ -1,55 +1,75 @@
 
-libraries<-c("lubridate","xts","forecast")
+#___________________________ IMPORTING DATA AND LIBRARIES __________________________________#
+libraries<-c("lubridate","forecast","tseries")
 sapply(libraries,require,character.only=TRUE)
 
-######################## IMPORTING DATA #################################
-train<-read.csv("C:/Users/20720/Desktop/AV-Time_Series/train.csv")
-test<-read.csv("C:/Users/20720/Desktop/AV-Time_Series/test.csv")
+train<-read.csv("F:/AnalyticsVidya/TimeSeriesAnalysis/train.csv")
+test<-read.csv("F:/AnalyticsVidya/TimeSeriesAnalysis/test.csv")
 
-######################### FORMATTING DATA ##############################
+#________________________________ FORMATTING DATA _____________________________________#
 
+#Lets have a look at the train data
+summary(train)
+str(train)
 
+#Coverting date time columns in to Datetime object
 train$Datetime<-dmy_hm(train$Datetime)
 test$Datetime<-dmy_hm(test$Datetime)
-#times.init <-format(train[,2], "%Y-%m-%d %H:%M:%S",tz="UTC",usetz=FALSE)
-#times.init<-as.POSIXct(times.init)
-#data2 <-zoo(train[,3],times.init)
-#data3 <-merge(data2, zoo(,seq(start(data2), end(data2),by="hour")))
+summary(train)
+str(train)
 
+dim(train)
+dim(test)
+#There are no missing observation in train data
+#However we are going to work only with 'Count' column 
 
-############## ARIMA ########################
-train_ts<-ts(train[,3],frequency=12)
-plot(train_ts)
-log_train_ts<-log(train_ts)
-plot(log_train_ts)
+#_____________________________________ MODELLING ________________________________________#
 
-ndiffs(log_train_ts, alpha=0.05, test=c("kpss","adf", "pp"), max.d=2)
-nsdiffs(log_train_ts, m=frequency(log_train_ts), test=c("ocsb","ch"), max.D=1)
+#Plotting first 240 data or first 
+temp_ts<-ts(train[10000:10167,3],frequency=24)
+ts.plot(temp_ts)
+#From the above plot we can easily decide that this timeseries has a 24 hour seasonal period
 
-diff1<-diff(log_train_ts,12)
-plot(diff1)
-adf.test(diff1)
+ts.plot(train[,3]) #graph shows that it has increasing variance.
 
-acf(diff1, lag.max=100)
-pacf(diff1,lag.max=100)
+#Taking log of the series
+timeseries<-ts(train[,3],frequency=24)
+log_timeseries<-log(timeseries)
+ts.plot(log_timeseries)
 
-fit <- Arima(log_train_ts, order=c(1,1,1))
+#As it is a seasonal time series we are taking seasonal difference
+diff_ts<-diff(log_train_ts,12)
+ts.plot(diff_ts)
+
+#lest look at the different metrics after taking seasonal difference
+acf(diff_ts,lag.max = 100)
+pacf(diff_ts,lag.max = 100)
+adf.test(diff_ts,k = 12)
+
+#Now apply auto arima to the train dataset
+fit <- auto.arima(timeseries, lambda=0)
 summary(fit)
 Acf(residuals(fit))
-temp<-Box.test(residuals(fit), lag=24, fitdf=4, type="Ljung")
+qqnorm(residuals(fit))
+qqline(residuals(fit))
+test<-Box.test(residuals(fit), lag=240, fitdf=24, type="Ljung")
 
-timeseries1<-auto.arima(log_train_ts,stepwise=FALSE)
-timeseries1_forcast<-forecast(timeseries1, h=50)
+#After calculation auto arima function chooses as the best fit.
+#but we know we have to take 1 seasonal difference.So->
+fit <- auto.arima(timeseries, lambda=0,d=0,D=1)
+summary(fit)
+Acf(residuals(fit))
+qqnorm(residuals(fit))
+qqline(residuals(fit))
+test<-Box.test(residuals(fit), lag=240, fitdf=24, type="Ljung")
 
-############ Holt's Exponential smoothing #####
+#Predicting future data
+timeseries1_forcast<-forecast(fit, h=5112)
 
-################## MSTS ###########################
-train_ts<-msts(train[,3],seasonal.periods=c(24,168))
-m_tbats = tbats(train_ts)
-f_tbats = forecast(m_tbats, h=5112)
-plot(f_tbats)
+#So best model predicted by arima is ARIMA(4,0,1)(2,1,0)[24]
 
-##################### Output ########################
-output<-data.frame(ID=test$ID,Count=round(f_tbats$mean))
 
-################# EXTRA ####################
+#___________________________________ OUTPUT ______________________________________#
+output<-data.frame(ID=test$ID,Count=timeseries1_forcast$mean)
+write.csv(output,"F:/AnalyticsVidya/TimeSeriesAnalysis/output.csv",row.names=FALSE)
+
